5bb6e9c20bc5be28436a73ec308322ff2a4ef002
UserDC <74959576+UserDC@users.noreply.github.com>
2023-09-22 19:57:02 +0200

    InserteHouses on phpge...
    Nao me lembro do resoto

---
+++ Pasta N1 V3 Db/phpages/InsertHouses.php
@@ -0,0 +1,43 @@
+<?php
+// Conectando ao banco de dados
+
+$conn = new mysqli("localhost", "root", "", "ElineDb");
+
+// Verifica se o formulário foi enviado
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Obtém os dados do formulário
+    $Owner = $_POST["Owner"];
+    $Price = $_POST["Price"];
+    
+    /* 
+    $nomeArquivo = $_FILES["arquivo"]["name"];
+    $tmpNomeArquivo = $_FILES["arquivo"]["tmp_name"];
+ */
+
+    // Move o arquivo para o diretório desejado (por exemplo, 'uploads/')
+    
+    
+/*
+     $diretorioDestino = 'uploads/';
+    move_uploaded_file($tmpNomeArquivo, $diretorioDestino . $nomeArquivo);
+ */
+
+    if ($conn->connect_error) {
+        die("Erro na conexão com a base de dados: " . $conn->connect_error);
+    }
+
+    $sql = "INSERT INTO houses (Owner, Price) VALUES ($Owner, $Price)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ss", $Owner, $Price);
+
+    if ($stmt->execute()) {
+        echo "Arquivo enviado e dados inseridos com sucesso na base de dados.";
+    } else {
+        echo "Erro ao inserir dados na base de dados: " . $conn->error;
+    }
+
+    // Feche a conexão com a base de dados
+    $stmt->close();
+    $conn->close();
+}
+?>
